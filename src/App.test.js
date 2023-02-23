64fabf9506057cca60eebfecb8082ecd67ac48c3
sagetamerta <38368716+sagetamerta@users.noreply.github.com>
2023-02-23 20:14:28 +0800

    fix icon and test for title

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
 	render(<App />);
-	const linkElement = screen.getByText(/Generate URL Public Discussion/i);
+	const linkElement = screen.getByText("Tools for Encrypt/Decrypt Text");
 	expect(linkElement).toBeInTheDocument();
 });
